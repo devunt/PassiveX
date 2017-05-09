eb3dca9d6ef0d86225499b4cba0c346be582225c
Junehyeon Bae <devunt@gmail.com>
2017-05-10 00:08:33 +0900

    Fix VeraportHandler to report all required and optional plugins as installed

--- PassiveX/Handlers/VeraportHandler.cs
+++ PassiveX/Handlers/VeraportHandler.cs
@@ -1,14 +1,17 @@
-﻿using Newtonsoft.Json;
+﻿using Flurl.Http;
+using Newtonsoft.Json;
+using Org.BouncyCastle.Cms;
 using System;
-using System.Reflection;
+using System.Linq;
 using System.Threading.Tasks;
+using System.Xml.Linq;
 
 namespace PassiveX.Handlers
 {
     [Handler(hostname: "127.0.0.1", port: 16105)]
     internal class VeraportHandler : IHandler
     {
-        public Task<HttpResponse> HandleRequest(HttpRequest request)
+        public async Task<HttpResponse> HandleRequest(HttpRequest request)
         {
             var response = new HttpResponse();
 
@@ -16,18 +19,24 @@ namespace PassiveX.Handlers
             {
                 var callback = request.Parameters["callback"];
                 var datafield = request.Parameters["data"];
-                dynamic data = JsonConvert.DeserializeObject(datafield);
+                dynamic rootData = JsonConvert.DeserializeObject(datafield);
+                dynamic data = rootData.data;
 
-                object result = null;
-
-                var method = GetType().GetMethod((string)data.cmd, BindingFlags.Instance | BindingFlags.NonPublic);
-                if (method != null)
+                dynamic result = null;
+                switch ((string) data.cmd)
                 {
-                    result = method.Invoke(this, null);
-                }
-                else
-                {
-                    Console.WriteLine($"Unknown command: {data.cmd}");
+                    case "getVersion":
+                        result = new { res = 0, data = "9,9,9,9" };
+                        break;
+                    case "isRunning":
+                        result = new { res = 0 };
+                        break;
+                    case "getAxInfo":
+                        result = new { res = 0, data = await GetAxInfoData((string)data.configure.axinfourl) };
+                        break;
+                    default:
+                        Console.WriteLine($"Unknown command: {data.cmd}");
+                        break;
                 }
 
                 var serialized = JsonConvert.SerializeObject(result);
@@ -35,73 +44,43 @@ namespace PassiveX.Handlers
                 response.Headers["Content-Type"] = "application/javascript";
             }
 
-            return Task.FromResult(response);
+            return response;
         }
 
-        private dynamic getVersion()
+        private async Task<dynamic> GetAxInfoData(string url)
         {
-            return new { res = 0, data = "9,9,9,9" };
-        }
+            var resp = await url.GetStringAsync();
+            var signedData = Convert.FromBase64String(resp);
+            var parser = new CmsSignedDataParser(signedData);
+            var stream = parser.GetSignedContent().ContentStream;
+            var doc = XDocument.Load(stream);
 
-        private dynamic getAxInfo()
-        {
-            return new
-            {
-                res = 0,
-                data = new[] {
-                    new {
-                        allowrun = true,
-                        allowrundomains = "",
-                        backupurl = "http://download.softforum.com/Published/AnySign/v1.1.0.7/AnySign_Installer.exe",
-                        block = false,
-                        browsertype = "Mozilla",
-                        browserversion = "-1",
-                        description = "https://www.shinhancard.com/solution/anysign/install/v1.1.0.5/AnySign_Installer.exe",
-                        displayname = "AnySign-multi",
-                        downloadurl = "http://download.softforum.com/Published/AnySign/v1.1.0.7/AnySign_Installer.exe",
-                        forceinstall = false,
-                        installstate = true,
-                        killbit = false,
-                        localversion = "9,9,9,9",
-                        objectclsid = "file:%ProgramFiles%\\Softforum\\XecureWeb\\AnySign\\dll\\AnySign4PCLauncher.exe",
-                        objectname = "AnySign",
-                        objecttype = 0,
-                        objectversion = "9.9.9.9",
-                        policydisable = false,
-                        systemtype = 0,
-                        updatestate = false,
-                        version = 5
-                    },
-                    new {
-                        allowrun = true,
-                        allowrundomains = "",
-                        backupurl = "https://supdate.nprotect.net/nprotect/nos_service/windows/install/nos_setup.exe",
-                        block = false,
-                        browsertype = "Mozilla",
-                        browserversion = "-1",
-                        description = "",
-                        displayname = "NOS-multi",
-                        downloadurl = "https://supdate.nprotect.net/nprotect/nos_service/windows/install/nos_setup.exe",
-                        forceinstall = false,
-                        installstate = true,
-                        killbit = false,
-                        localversion = "9999,99,9,9",
-                        objectclsid = "file:%ProgramFiles%\\INCAInternet\\nProtect Online Security\\nosstarter.npe",
-                        objectname = "NOS",
-                        objecttype = 0,
-                        objectversion = "",
-                        policydisable = false,
-                        systemtype = 0,
-                        updatestate = false,
-                        version = 5
-                    },
-                },
-            };
-        }
-
-        private dynamic isRunning()
-        {
-            return new { res = 0, data = 0 };
+            return doc.Root.Elements("object")
+                .GroupBy(x => x.Element("objectName").Value)
+                .Select(g => g.First())
+                .Select(x => new {
+                    allowrun = true,
+                    allowrundomains = "",
+                    backupurl = x.Element("backupURL").Value,
+                    block = false,
+                    browsertype = x.Element("browserType").Value,
+                    browserversion = -1,
+                    description = "",
+                    displayname = x.Element("displayName").Value,
+                    downloadurl = x.Element("downloadURL").Value,
+                    forceinstall = false,
+                    installstate = true,
+                    killbit = false,
+                    localversion = x.Element("objectVersion").Value,
+                    objectclsid = x.Element("objectMIMEType").Value,
+                    objectname = x.Element("objectName").Value,
+                    objecttype = 0,
+                    objectversion = x.Element("objectVersion").Value,
+                    policydisable = false,
+                    systemtype = x.Element("systemType").Value,
+                    updatestate = false,
+                    version = 0,
+                });
         }
     }
 }
