3f90d24c7ecaa8f23cc2b0d5d352ddcbe233c035
Junehyeon Bae <devunt@gmail.com>
2017-05-11 05:06:42 +0900

    Add makecert.exe command line that was used to generate root ca certificate

--- PassiveX/Program.cs
+++ PassiveX/Program.cs
@@ -8,6 +8,11 @@ namespace PassiveX
     {
         static async Task AsyncMain()
         {
+            /*
+             * makecert.exe -r -pe -n "CN=! PassiveX Root CA" -sv ca.pvk -a sha256 -len 2048 -b 01/01/2000 -e 12/31/2039 -cy authority ca.cer
+             * pvk2pfx -pvk ca.pvk -spc ca.cer -pfx ca.pfx
+             */
+
             CertificateBuilder.Initialize(@"Resources/ca.pfx");
             CertificateBuilder.Install();
 
